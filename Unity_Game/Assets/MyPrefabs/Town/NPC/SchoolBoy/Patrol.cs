feca35d8e3c8fc18a1430a99e3bc6abb33d3ee0f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-11 01:21:01 +0200

    Working with NPC and patrolling

---
+++ Unity_Game/Assets/MyPrefabs/Town/NPC/SchoolBoy/Patrol.cs
@@ -0,0 +1,48 @@
+// Patrol.cs
+
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MyPrefabs.Town.NPC.SchoolBoy
+{
+    public class Patrol : MonoBehaviour
+    {
+        public Transform[] points;
+        private int _destPoint;
+        public NavMeshAgent agent;
+
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+
+            // Disabling auto-braking allows for continuous movement
+            // between points (ie, the agent doesn't slow down as it
+            // approaches a destination point).
+            agent.autoBraking = false;
+
+            GotoNextPoint();
+        }
+
+        private void GotoNextPoint()
+        {
+            // Returns if no points have been set up
+            if (points.Length == 0)
+                return;
+
+            // Set the agent to go to the currently selected destination.
+            agent.destination = points[_destPoint].position;
+
+            // Choose the next point in the array as the destination,
+            // cycling to the start if necessary.
+            _destPoint = (_destPoint + 1) % points.Length;
+        }
+
+        private void Update()
+        {
+            // Choose the next destination point when the agent gets
+            // close to the current one.
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+    }
+}
