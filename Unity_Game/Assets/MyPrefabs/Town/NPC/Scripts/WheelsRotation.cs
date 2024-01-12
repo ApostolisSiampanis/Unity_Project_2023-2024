764a579c384047878fc7f9763b7c387fed52dfb6
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-12 19:36:35 +0200

    Add wheels rotation on movement

--- Unity_Game/Assets/MyPrefabs/Town/NPC/Scripts/WheelsRotation.cs
+++ Unity_Game/Assets/MyPrefabs/Town/NPC/Scripts/WheelsRotation.cs
@@ -1,23 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Wheels : MonoBehaviour
+namespace MyPrefabs.Town.NPC.Scripts
 {
-    [SerializeField] private Transform[] wheels;
-    // Start is called before the first frame update
-    void Start()
+    public class Wheels : MonoBehaviour
     {
+        [SerializeField] private Transform[] wheels;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // warning This is a hack.  It should be done with an animation.
-        foreach (var wheel in wheels)
+        private void Update()
         {
-            wheel.Rotate(Vector3.right, 1000 * Time.deltaTime);
+            foreach (var wheel in wheels)
+            {
+                wheel.Rotate(Vector3.right, 300 * Time.deltaTime);
+            }
         }
     }
 }
