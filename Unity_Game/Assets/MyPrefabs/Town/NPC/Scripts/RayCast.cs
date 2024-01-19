ae75019bbb5065569c2e13363f6bed745fe19038
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-19 21:24:45 +0200

    update RayCast.cs

--- Unity_Game/Assets/MyPrefabs/Town/NPC/Scripts/RayCast.cs
+++ Unity_Game/Assets/MyPrefabs/Town/NPC/Scripts/RayCast.cs
@@ -7,28 +7,29 @@ namespace MyPrefabs.Town.NPC.Scripts
         [SerializeField] private Transform alexTransform;
         [SerializeField] private Animator npcAnimator;
         private static readonly int WAVE = Animator.StringToHash("wave");
-        private Vector3 m_npcPosition, m_npcEyesPosition;
+        private Vector3 m_npcEyesPosition, m_alexEyesPosition;
         private bool m_alreadyWaved;
 
         private void Start()
         {
             npcAnimator = GetComponent<Animator>();
             alexTransform = GameObject.FindWithTag("Player").transform;
-            m_npcPosition = transform.position + new Vector3(0, 1.7f, 0);
+            m_npcEyesPosition = transform.position + new Vector3(0, 1.7f, 0);
         }
 
         private void FixedUpdate()
         {
-            m_npcEyesPosition = alexTransform.position + new Vector3(0, 1.65f, 0);
+            m_alexEyesPosition = alexTransform.position + new Vector3(0, 1.65f, 0);
 
-            Vector3 direction = m_npcEyesPosition - m_npcPosition;
+            Vector3 direction = m_alexEyesPosition - m_npcEyesPosition;
 
-            Debug.DrawRay(m_npcPosition, direction, Color.yellow);
+            Debug.DrawRay(m_npcEyesPosition, direction, Color.yellow);
 
-            if (!Physics.Raycast(m_npcPosition, direction, out var hit, 10f)) return;
+            if (!Physics.Raycast(m_npcEyesPosition, direction, out var hit, 10f)) return;
             if (hit.collider.transform == alexTransform)
             {
                 if (m_alreadyWaved) return;
+                transform.LookAt(alexTransform);
                 npcAnimator.SetTrigger(WAVE);
                 m_alreadyWaved = true;
             }
