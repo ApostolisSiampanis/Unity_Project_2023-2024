86f7babb66b8a21b2ec47311e50b4b00cf8e2f21
Anthony Roussos <github.eloquent385@passinbox.com>
2024-03-11 18:39:10 +0200

    Update NPCSpeaker.cs

--- Unity_Game/Assets/Farm/Scripts/Interaction System/NPCSpeaker.cs
+++ Unity_Game/Assets/Farm/Scripts/Interaction System/NPCSpeaker.cs
@@ -1,40 +1,65 @@
+using Farm.Scripts.DialogueSystem;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Farm.Scripts.Interaction_System
 {
-    public class NPCSpeaker : MonoBehaviour, Interactable
+    public class NPCSpeaker : MonoBehaviour, Interactable, ISpeak
     {
-
+        // ====== INTERACTION ====== //
         [Header("Interaction")]
         [SerializeField] private string _taskHint = "talk to Bob";
         [SerializeField] private KeyCode _interactKey;
         [SerializeField] private bool _readyToInteract = true;
 
         private Interactor _interactor;
+        private DialogueTrigger _dialogueTrigger;
+        
+        private Animator _animator;
+        private NavMeshAgent _navMeshAgent;
 
-        public TaskStatus Status { get; set; }
+        private Quaternion _prevRotation;
+
+        private bool _isTalking;
 
         public void Start()
         {
-            Status = TaskStatus.NOT_STARTED;
+            _isTalking = false;
+            
+            _dialogueTrigger = GetComponent<DialogueTrigger>();
+            _animator = GetComponent<Animator>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            
+            // Check if necessary objects are present
+            if (_dialogueTrigger == null) Debug.LogError("DialogueTrigger is missing");
+            if (_animator == null) Debug.LogError("Animator is missing");
+            if (_navMeshAgent == null) Debug.LogError("NavMeshAgent is missing");
+            
         }
 
         public void OnInteract(Interactor interactor)
         {
             _interactor = interactor;
-            Status = TaskStatus.IN_PROCESS;
-            //
-            // Status = TaskStatus.COMPLETED;
-            // _interactor.EndInteraction(this);
-        }
 
-        // TODO: Change return type to boolean (or TaskStatus) for task complete status
-        public TaskStatus OnEndInteract()
+            if (_dialogueTrigger == null) return;
+            
+            _isTalking = true;
+            ChangeState();
+            _dialogueTrigger.TriggerDialogue(this);
+            
+        }
+        
+        public void OnEndInteract()
         {
-            if (Status == TaskStatus.IN_PROCESS) Status = TaskStatus.ABORTED;
+            if (_isTalking)
+            {
+                _isTalking = false;
+                _dialogueTrigger.Abort();
+                Debug.Log("Abort dialogue.");
+            }
             Debug.Log("End of interaction.");
-            return Status;
+            ChangeState();
         }
 
         public bool IsReadyToInteract([CanBeNull] out string taskHint, out KeyCode interactKey)
@@ -47,7 +72,33 @@ namespace Farm.Scripts.Interaction_System
 
         public void OnAbortInteract()
         {
-            Debug.Log("Abort interact");
+            Debug.Log("Abort interaction");
+        }
+
+        public void OnDialogueEnd(bool wasFinished)
+        {
+            if (!_isTalking || !wasFinished) return;
+            _interactor.EndInteraction(this);
+            Debug.Log("Dialog finished successfully.");
+        }
+
+        private void ChangeState()
+        {
+            _animator.SetBool("isWalking", !_isTalking);
+            _navMeshAgent.isStopped = _isTalking;
+
+            if (_isTalking)
+            {
+                // Change orientation to look at the interactor
+                _prevRotation = transform.rotation;
+                var direction = _interactor.transform.position - transform.position;
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+            else
+            {
+                // Change the orientation back to normal
+                transform.rotation = _prevRotation;
+            }
         }
     }
 }
