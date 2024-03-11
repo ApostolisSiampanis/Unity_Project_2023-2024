5c6eb383b651b0b0b2d25d1a519a0ad39fd2634f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-03-11 15:32:24 +0200

    Update script for hiding the crosshair and the minimap when playing timeline in forest scene

--- Unity_Game/Assets/MyPrefabs/Forest/Scripts/GameCanvas.cs
+++ Unity_Game/Assets/MyPrefabs/Forest/Scripts/GameCanvas.cs
@@ -7,14 +7,20 @@ namespace MyPrefabs.Forest.Scripts
     {
         [SerializeField] private PlayableDirector playableDirector1;
         [SerializeField] private PlayableDirector playableDirector2;
+        [SerializeField] private PlayableDirector playableDirector3;
         [SerializeField] private GameObject crosshair;
         [SerializeField] private GameObject miniMap;
+        [SerializeField] private GameObject alex;
 
         private void Update()
         {
-            bool isEnabled = playableDirector1.enabled || playableDirector2.enabled;
+            // Disable crosshair and minimap when playable directors are enabled.
+            bool isEnabled = playableDirector1.enabled || playableDirector2.enabled || playableDirector3.enabled;
             crosshair.SetActive(!isEnabled);
             miniMap.SetActive(!isEnabled);
+
+            // Disable the farm-forest playable director when Alex is active.
+            if (alex.activeInHierarchy) playableDirector1.enabled = false;
         }
     }
 }
