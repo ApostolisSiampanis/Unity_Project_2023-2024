61e2e7c3c2d545c3b79928bc5e61b57c6c4ae30f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-03-16 00:10:40 +0200

    Update localization for town scene

---
+++ Unity_Game/Assets/Scripts/Town/Quests/BeforeQuests.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using Common.QuestSystem;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
+
+namespace Town.Quests
+{
+    public class BeforeQuests : MonoBehaviour
+    {
+        [SerializeField] private Quest quest;
+        [SerializeField] private LocalizeStringEvent localizeStringEvent;
+        [SerializeField] private string titleKey;
+        [SerializeField] private string descriptionKey;
+
+        private void Start()
+        {
+            // Wait for the localization to be initialized.
+            StartCoroutine(InitLocalization());
+
+            // Get the localized strings.
+            const string table = "BeforeQuestsTownTable";
+
+            // Quest title.
+            localizeStringEvent.StringReference.SetReference(table, titleKey);
+            string title = localizeStringEvent.StringReference.GetLocalizedString();
+
+            // Quest description.
+            localizeStringEvent.StringReference.SetReference(table, descriptionKey);
+            string description = localizeStringEvent.StringReference.GetLocalizedString();
+
+            // Set the title and description.
+            quest.title = title;
+            quest.description = description;
+
+            // Update the quest UI.
+            QuestManager.instance.UpdateQuestUI();
+        }
+
+        private static IEnumerator InitLocalization()
+        {
+            yield return LocalizationSettings.InitializationOperation;
+        }
+    }
+}
